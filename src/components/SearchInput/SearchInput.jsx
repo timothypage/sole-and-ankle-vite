373cb6c1219c649bfefd621e31a468bae5656bfc
Tim Zwolak <timothypage@gmail.com>
2023-07-10 13:51:05 -0600

    style SearchInput

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,5 +1,13 @@
-const SearchInput = () => (
-  <div>Search</div>
+import { Search } from "react-feather";
+
+import styles from "./SearchInput.module.css"
+
+const SearchInput = ({ label, ...delegated }) => (
+  <label className={styles.label}>
+    <p className="visually-hidden">Search</p>
+    <input className={styles.input} placeholder="Search..." {...delegated} />
+    <Search className={styles.icon} size={16} />
+  </label>
 )
 
 export default SearchInput
