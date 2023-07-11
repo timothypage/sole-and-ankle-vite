2a6ae1c9674874f10db621a8ff12387ecacb1bd9
Tim Zwolak <timothypage@gmail.com>
2023-07-11 10:41:09 -0600

    style ShoeList

--- src/components/ShoeList/ShoeList.jsx
+++ src/components/ShoeList/ShoeList.jsx
@@ -1,49 +1,15 @@
 import classnames from 'classnames'
 import ShoeItem from '/src/components/ShoeItem'
 
-import styles from "./ShoeList.module.css"
+import shoes from '/src/data/shoes'
+import styles from './ShoeList.module.css'
 
 const ShoeList = ({ className }) => {
-  const shoes = [
-    {
-      name: 'Boom Goes the Dynamite', 
-      type: 'Basketball', 
-      price: 165,
-      isNew: true,
-      colors: 2,
-      image: 'basketball-boom-goes-the-dynamite.png' 
-    },
-    {
-      name: 'El Tigre',
-      type: 'basketball',
-      price: 165,
-      salePrice: 145,
-      colors: 1,
-      image: 'basketball-el-tigre.png'
-    },
-    {
-      name: 'Emperor',
-      type: 'basketball',
-      price: 165,
-      isNew: true,
-      colors: 4,
-      image: 'basketball-emperor.png'
-    },
-    {
-      name: 'Jicama',
-      type: 'basketball',
-      price: 165,
-      colors: 2,
-      image: 'basketball-jicama.png'
-    }
-  ];
-
   return (
-    <div className={classnames(className)}>
+    <div className={classnames(className, styles.shoeList)}>
       {shoes.map(shoe => (
-        <ShoeItem key={shoe.name} {...shoe} />
-      ))
-      }
+        <ShoeItem key={shoe.name} className={styles.shoeItem} {...shoe} />
+      ))}
     </div>
   )
 }
---
+++ src/data/shoes.js
@@ -0,0 +1,165 @@
+const shoes = [
+  {
+    name: 'Boom Goes the Dynamite',
+    type: 'Basketball',
+    price: 165,
+    isNew: true,
+    colors: 2,
+    image: 'basketball-boom-goes-the-dynamite.png'
+  },
+  {
+    name: 'El Tigre',
+    type: 'basketball',
+    price: 165,
+    salePrice: 145,
+    colors: 1,
+    image: 'basketball-el-tigre.png'
+  },
+  {
+    name: 'Emperor',
+    type: 'basketball',
+    price: 165,
+    isNew: true,
+    colors: 4,
+    image: 'basketball-emperor.png'
+  },
+  {
+    name: 'Jicama',
+    type: 'basketball',
+    price: 165,
+    colors: 2,
+    image: 'basketball-jicama.png'
+  },
+  {
+    name: 'Elixir',
+    type: 'cleat',
+    price: 125,
+    colors: 3,
+    image: 'cleat-elixir.png'
+  },
+  {
+    name: 'Haskell',
+    type: 'cleat',
+    price: 105,
+    colors: 2,
+    image: 'cleat-haskell.png'
+  },
+  {
+    name: 'Python',
+    type: 'cleat',
+    price: 145,
+    colors: 1,
+    image: 'cleat-python.png'
+  },
+  {
+    name: 'Ruby',
+    type: 'cleat',
+    price: 185,
+    colors: 1,
+    image: 'cleat-ruby.png'
+  },
+  {
+    name: 'Alpharetta',
+    type: 'dress',
+    price: 235,
+    colors: 1,
+    image: 'dress-shoe-alpharetta.png'
+  },
+  {
+    name: 'C Suite',
+    type: 'dress',
+    price: 265,
+    colors: 2,
+    image: 'dress-shoe-c-suite.png'
+  },
+  {
+    name: 'Harvard',
+    type: 'dress',
+    price: 325,
+    colors: 1,
+    image: 'dress-shoe-harvard.png'
+  },
+  {
+    name: 'Oxford',
+    type: 'dress',
+    price: 205,
+    salePrice: 180,
+    colors: 2,
+    image: 'dress-shoe-oxford.png'
+  },
+  {
+    name: 'Bierstadt',
+    type: 'hiking',
+    price: 235,
+    colors: 4,
+    image: 'hiking-boot-bierstadt.png'
+  },
+  {
+    name: 'Elbert',
+    type: 'hiking',
+    price: 265,
+    salePrice: 235,
+    colors: 2,
+    image: 'hiking-boot-elbert.png'
+  },
+  {
+    name: 'Thorodin',
+    type: 'hiking',
+    price: 235,
+    colors: 1,
+    image: 'hiking-boot-thorodin.png'
+  },
+  {
+    name: 'Shasta',
+    type: 'hiking',
+    price: 165,
+    colors: 2,
+    image: 'hiking-boot-shasta.png'
+  },
+  {
+    name: 'Cooper',
+    type: 'sneaker',
+    price: 85,
+    colors: 1,
+    image: 'sneaker-cooper.png'
+  },
+  {
+    name: 'Oppenheimer',
+    type: 'sneaker',
+    price: 75,
+    colors: 2,
+    image: 'sneaker-oppenheimer.png'
+  },
+  {
+    name: 'Sheffield',
+    type: 'sneaker',
+    price: 95,
+    salePrice: 85,
+    colors: 3,
+    image: 'sneaker-sheffield.png'
+  },
+  {
+    name: 'Caicos',
+    type: 'sandal',
+    price: 115,
+    colors: 2,
+    image: 'sandal-caicos.png'
+  },
+  {
+    name: 'Monaco',
+    type: 'sandal',
+    price: 105,
+    salePrice: 75,
+    colors: 1,
+    image: 'sandal-monaco.png'
+  },
+  {
+    name: 'Nassau',
+    type: 'sandal',
+    price: 95,
+    colors: 3,
+    image: 'sandal-nassau.png'
+  }
+]
+
+export default shoes
